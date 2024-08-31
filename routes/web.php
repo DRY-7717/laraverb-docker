378764920da8b2a7a26a2af2c25d7006df7e46c3
DRY-7717 <wicaksanabimaarya01@gmail.com>
2024-09-01 05:57:02 +0700

    adding configuration for laravel reverb

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Events\ExampleEvent;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/broadcast', function () {
+    broadcast(new ExampleEvent());
 });
 
 Route::get('/dashboard', function () {
@@ -16,5 +21,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
